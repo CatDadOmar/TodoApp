0677c092c08471184d213c227132a7974b2dec7f
CatDadOmar <omarabdullakord@gmail.com>
2024-10-29 18:33:36 +0100

    k

--- app/src/main/java/com/example/todoapp/TaskDatabase.kt
+++ app/src/main/java/com/example/todoapp/TaskDatabase.kt
@@ -1,2 +1,25 @@
 package com.example.todoapp
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): Task
+
+    companion object {
+        @Volatile private var instance: TaskDatabase? = null
+
+        fun getDatabase(context: Context): TaskDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDatabase::class.java,
+                    "task_database"
+                ).build().also { instance = it }
+            }
+        }
+    }
+}
